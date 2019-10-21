1bd1472d33a216f54b54fdff63dde43de7b8a0bc
systena <systena@DESKTOP-564VDM7>
2019-10-21 19:19:42 +0900

    作成

---
+++ src/test/java/jp/co/systena/tigerscave/Shopping/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.systena.tigerscave.Shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
